76b70aa634770b4e2b84170246cee7c06e7f3b75
Rob Loach <robloach@gmail.com>
2016-05-22 09:59:53 -0400

    Update the user interface

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 -- Configuration
 function lutro.conf(t)
-  -- Application metadata
-  t.title = 'Lutro'
-  t.author = 'Rob Loach'
-  t.version = '0.0.1'
-  t.identity = 'lutris-lutro'
-	t.width = 900
-	t.height = 600
+	-- Application metadata
+	t.title = 'Lutro'
+	t.author = 'Rob Loach'
+	t.version = '0.0.1'
+	t.identity = 'lutris-lutro'
+	t.width = 1280
+	t.height = 720
 end
