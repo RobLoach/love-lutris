1b1401125e253bbc125a3d0607a4e7e2fdc52642
Rob Loach <robloach@gmail.com>
2016-06-07 03:21:49 -0400

    Add Love2D Support

--- conf.lua
+++ conf.lua
@@ -1,10 +1,18 @@
+lib = nil
+if (love ~= nil) then
+  lib = love
+elseif (lutro ~= nil) then
+  lib = lutro
+end
+
 -- Configuration
-function lutro.conf(t)
+function lib.conf(t)
 	-- Application metadata
-	t.title = 'Lutro'
+	t.title = 'Lutris'
 	t.author = 'Rob Loach'
-	t.version = '0.0.1'
-	t.identity = 'lutris-lutro'
+	t.version = '0.10.1'
+	t.identity = 'lutro-lutris'
+	t.description = "Lutris game launcher for RetroArch/Love2D"
 	t.width = 1280
 	t.height = 720
 end
--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
+lib = nil
+if (love ~= nil) then
+  lib = love
+elseif (lutro ~= nil) then
+  lib = lutro
+end
+
 -- Dependencies
 require('vendor')
 require('conf')
 
 -- Load
-function lutro.load()
+function lib.load()
 	-- Load the available games
 	local success, result = pcall(getInstalledGames)
 	if success then
@@ -19,9 +26,9 @@ function lutro.load()
 	})
 
 	-- Initialization
-	font = lutro.graphics.newImageFont('resources/font.png', ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/')
-	lutro.graphics.setFont(font)
-	lutro.graphics.setBackgroundColor(1, 53, 146)
+	font = lib.graphics.newImageFont('resources/font.png', ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/')
+	lib.graphics.setFont(font)
+	lib.graphics.setBackgroundColor(1, 53, 146)
 
 	lineHeight = 20
 	currentSelection = 1
@@ -29,54 +36,76 @@ function lutro.load()
 	keyHoldResetTime = 1000
 	keySpeed = 5000
 
-	logo = lutro.graphics.newImage('resources/logo.png')
+	logo = lib.graphics.newImage('resources/logo.png')
 end
 
 -- Draw
-function lutro.draw()
-	lutro.graphics.clear()
-	lutro.graphics.draw(logo, lineHeight, lineHeight)
+function lib.draw()
+	lib.graphics.clear()
+	lib.graphics.draw(logo, lineHeight, lineHeight)
 
 	-- Show each game.
 	local y = lineHeight * 2
 	for i, game in ipairs(games) do
 		-- Display the menu cursor if needed
 		if currentSelection == i then
-			lutro.graphics.rectangle('fill', 256 + lineHeight * 2, y + lineHeight / 4, lineHeight / 3, lineHeight / 3)
+			lib.graphics.rectangle('fill', 256 + lineHeight * 2, y + lineHeight / 4, lineHeight / 3, lineHeight / 3)
 		end
 		-- Display the menu item
-		lutro.graphics.print(game.name, 256 + lineHeight * 3, y)
+		lib.graphics.print(game.name, 256 + lineHeight * 3, y)
 		y = y + lineHeight
 	end
 end
 
 -- Update
-function lutro.update(dt)
-	-- Up
-	if lutro.input.joypad("up") == 1 and keyHoldTimer <= 0 then
-		currentSelection = currentSelection - 1
-		keyHoldTimer = keyHoldResetTime
-		if currentSelection < 1 then
-			currentSelection = 1
+if (lutro ~= nil) then
+	function lutro.update(dt)
+		-- Up
+		if lib.input.joypad("up") == 1 and keyHoldTimer <= 0 then
+			currentSelection = currentSelection - 1
+			keyHoldTimer = keyHoldResetTime
+			if currentSelection < 1 then
+				currentSelection = 1
+			end
+		-- Down
+		elseif lib.input.joypad("down") == 1 and keyHoldTimer <= 0 then
+			currentSelection = currentSelection + 1
+			keyHoldTimer = keyHoldResetTime
+			if currentSelection > table.getn(games) then
+				currentSelection = table.getn(games)
+			end
+		-- Select
+		elseif (lib.input.joypad("start") == 1 or lib.input.joypad("b") == 1 or lib.input.joypad("y") == 1 or lib.input.joypad("a") == 1 or lib.input.joypad("x") == 1) and keyHoldTimer <= 0 then
+			keyHoldTimer = keyHoldResetTime * 2
+			game = games[currentSelection]
+			launchGame(game.slug)
+		else
+			keyHoldTimer = keyHoldTimer - dt * keySpeed
 		end
-	-- Down
-	elseif lutro.input.joypad("down") == 1 and keyHoldTimer <= 0 then
-		currentSelection = currentSelection + 1
-		keyHoldTimer = keyHoldResetTime
-		if currentSelection > table.getn(games) then
-			currentSelection = table.getn(games)
-		end
-	-- Select
-	elseif (lutro.input.joypad("start") == 1 or lutro.input.joypad("b") == 1 or lutro.input.joypad("y") == 1 or lutro.input.joypad("a") == 1 or lutro.input.joypad("x") == 1) and keyHoldTimer <= 0 then
-		keyHoldTimer = keyHoldResetTime * 2
-		game = games[currentSelection]
-		--os.execute('lutris lutris:' .. game.slug)
-		local f = io.popen('lutris lutris:' .. game.slug)
-		f:close()
-	-- Nothing
-	else
-		keyHoldTimer = keyHoldTimer - dt * keySpeed
 	end
+else
+	function love.keypressed(key)
+		if key == "down" then
+			currentSelection = currentSelection + 1
+			if currentSelection > table.getn(games) then
+				currentSelection = table.getn(games)
+			end
+		elseif key == "up" then
+			currentSelection = currentSelection - 1
+			if currentSelection < 1 then
+				currentSelection = 1
+			end
+		elseif key == "return" then
+			game = games[currentSelection]
+			launchGame(game.slug)
+		end
+	end
+end
+
+function launchGame(slug)
+	--os.execute('lutris lutris:' .. game.slug)
+	local f = io.popen('lutris lutris:' .. slug)
+	f:close()
 end
 
 -- Retrieve the installed Lutro games
