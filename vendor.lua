cffed5a010fea0d0d2a526e2b949d8b2e4dbe3e5
Rob Loach <robloach@gmail.com>
2016-06-27 16:32:19 -0400

    Fix the vendor loading paths - fixes #9

--- vendor.lua
+++ vendor.lua
@@ -1,10 +1,14 @@
 -- Configures the package path to allow loading vendor paths.
+
+-- Find the current source directory.
 local dirname = debug.getinfo(1).source:match("@?(.*/)")
+
+-- If the directory is available, add it using package.path.
 if dirname then
 	package.path = package.path .. ';' .. dirname .. '/vendor/lunajson/src/?.lua'
 else
-	package.path = package.path .. ';' .. love.filesystem.getSourceBaseDirectory() .. 'vendor/lunajson/src/?.lua'
+	-- If the current source directory isn't available, use the Love API.
+	local paths = love.filesystem.getRequirePath()
+	paths = paths .. ';vendor/lunajson/src/?.lua'
+	love.filesystem.setRequirePath(paths)
 end
-
---package.path = debug.getinfo(1, "S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
-
