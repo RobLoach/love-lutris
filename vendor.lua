80ea907e9abd4f0badfbe6a6f135f350f7690f3f
Rob Loach <robloach@gmail.com>
2016-06-27 20:51:36 -0400

    Add love.filesystem.setRequirePath() usage to vendor paths

--- vendor.lua
+++ vendor.lua
@@ -1,14 +1,22 @@
 -- Configures the package path to allow loading vendor paths.
-
--- Find the current source directory.
-local dirname = debug.getinfo(1).source:match("@?(.*/)")
-
--- If the directory is available, add it using package.path.
-if dirname then
-	package.path = package.path .. ';' .. dirname .. '/vendor/lunajson/src/?.lua'
-else
-	-- If the current source directory isn't available, use the Love API.
-	local paths = love.filesystem.getRequirePath()
-	paths = paths .. ';vendor/lunajson/src/?.lua'
-	love.filesystem.setRequirePath(paths)
+local lib = nil
+if (love ~= nil) then
+  lib = love
+elseif (lutro ~= nil) then
+  lib = lutro
 end
+
+-- Find the current source directory to append the vendor paths.
+local paths = lib.filesystem.getRequirePath()
+
+-- LunaJSON
+paths = paths .. ';vendor/lunajson/src/?.lua'
+
+-- If the dirname is available, append it to the paths too.
+local dirname = debug.getinfo(1).source:match("@?(.*/)")
+if dirname then
+	paths = paths .. ';' .. dirname .. '/vendor/lunajson/src/?.lua'
+end
+
+-- Finally, add the require paths.
+lib.filesystem.setRequirePath(paths)
